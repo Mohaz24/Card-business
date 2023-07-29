176fea30b58badc36c8214be4469e603b9a532fe
Mohaz <107796482+Mohaz24@users.noreply.github.com>
2023-07-29 17:17:31 +1000

    compelete

---
+++ App.js
@@ -0,0 +1,17 @@
+import  React from "react"
+import  ReactDOM from "react-dom"
+import  Header from "./Header.js"
+import  Footer from "./Footer.js"
+ 
+ 
+ function App(){
+     return (
+         <div>
+            <Header />
+            <Footer />
+        </div>
+     )
+ }
+ 
+ 
+ export default App
---
+++ Interest.js
@@ -0,0 +1,13 @@
+import  React from "react"
+import  ReactDOM from "react-dom"
+
+function Interest(){
+    return (
+     <div className="interest--content">
+        <h2 className="interest--title">Interests</h2>
+        <p className="interest--text">Food expert. Music scholar. Reader. Internet fanatic. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>   
+     </div>
+    )
+}
+
+export default Interest
